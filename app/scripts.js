813cebeebef9c8a754ad08141a4fe7469fe1a94b
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-20 13:30:28 -0600

    add list

--- app/scripts.js
+++ app/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   var listo = [];
 
+      $('#newTaskForm').hide();
+
   var Task = function(task) {
     this.task = task;
     this.id = 'new';
@@ -24,4 +26,65 @@ $(document).ready(function(){
         '</a>'
       );
   };
+  $('#newTaskForm').slideToggle('fast', 'linear');
+  //slides the window up
+  $('#saveNewItem').on('click', function(e){
+    e.preventDefault();
+    var task = $('#newItemInput').val().trim();
+    add(task);
+  });
+  //
+  $('#add-todo').on('click', function(){
+    $('#newTaskForm').fadeToggle('fast', 'linear');
+  });
+  //click on the add button to slide window up
+  $('#cancel').on('click', function(e){
+    e.preventDefault();
+    $('#newTaskForm').fadeToggle('fast', 'linear');
+  });
+  //make the window slide down when X is clicked
+
+  var advanceTask = function(task) {
+    var modified = task.innerText.trim();
+    for(var i = 0; i<listo.length; i++) {
+      if(listo[i].task === modified){
+        if(listo[i].id === 'new'){
+          listo[i].id = 'inprogress';
+        } else if (listo[i].id === 'inProgress'){
+          listo[i].id = 'archived';
+        } else {
+          listo.splice(i, 1);
+        }
+        break;
+      }
+    }
+    task.remove();
+  };
+
+  $(document).on('click', '#item', function(e){
+    e.preventDefault();
+  });
+
+  $(document).on('click', '#item', function(e) {
+    e.preventDefault();
+    var task = this;
+    advanceTask(task);
+    this.id = 'inProgress';
+    $('#currentList').append(this.outerHTML);
+  });
+
+  $(document).on('click', '#inProgress', function (e) {
+    e.preventDefault();
+    var task =this;
+    task.id = "arhived";
+    var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+    advanceTask(task);
+    $('#archivedList').append(changeIcon);
+  });
+
+  $(document).on('click', "#archived", function (e){
+    e.preventDefault();
+    var task = this;
+    advancedTask(task);
+  });
 });
